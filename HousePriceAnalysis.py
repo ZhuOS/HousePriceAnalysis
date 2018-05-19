a663c20c0e3c22de4d642f92fa887f51cc2d9578
ZhuOS <908091822@qq.com>
2018-05-19 19:53:31 +0800

    implement district information updating

--- HousePriceAnalysis.py
+++ HousePriceAnalysis.py
@@ -66,15 +66,16 @@ Update city_info table
 ANJUKE_CITY_INFO_URL = 'https://www.anjuke.com/sy-city.html'
 CITY_INFO_TABLE = 'city_info'
 
-def UpdateTable_city_info(DB, Logger, url = ANJUKE_CITY_INFO_URL):
+def UpdateCityInfoFromAnjuke(DB, Logger, url = ANJUKE_CITY_INFO_URL):
 	try:
 		city_homepage_url_dict = GetAnjukeCityUrl(url)
 		city_rent_url_dict = GetAnjukeCityZuUrl(city_homepage_url_dict)
 	except Exception as err:
 		trace = traceback.format_exc()
 		msg = "[Exception] %s\n%s" % (str(err), trace);
-		print(f'Error update table city_info!')
+		print(f'Error Get city_info!')
 		Logger.error(msg)
+		return
 				
 	for city, rent_url in city_rent_url_dict.items():
 		fields = {}
@@ -91,7 +92,7 @@ def UpdateTable_city_info(DB, Logger, url = ANJUKE_CITY_INFO_URL):
 				Logger.info("insert table[%s] city_name[%s], anjuke_homepage_url[%s] anjuke_rent_url[%s] ok!" % (CITY_INFO_TABLE, fields["city_name"], fields['anjuke_homepage_url'], fields['anjuke_rent_url']));            
 			else:            
 				Logger.error("insert table[%s] city_info city_name[%s], anjuke_homepage_url[%s] anjuke_rent_url[%s] failed! error[%s]" \
-							   % (CITY_INFO_TABLE, fields["city_name"], fields['anjuke_homepage_url'], fields['anjuke_rent_url'], DB.get_last_error()));
+				% (CITY_INFO_TABLE, fields["city_name"], fields['anjuke_homepage_url'], fields['anjuke_rent_url'], DB.get_last_error()));
 				continue
 		else:
 			city_id = rows[0]['city_id']
@@ -112,7 +113,7 @@ def GetAnjukeCityUrl(url = ANJUKE_CITY_INFO_URL):
 			city = city_item_soup.getText().strip()
 			c_url = city_item_soup['href']
 			citys_url_dict[city] = c_url
-			break
+			
 	return citys_url_dict
 
 def GetAnjukeCityZuUrl(city_url_dict):
@@ -137,19 +138,66 @@ def GetAnjukeCityZuUrl(city_url_dict):
 		print(f'{city}-{c_url}-{zu_url}')
 	return city_zuurl_dict
 	
+'''
+Update district_info table'
+'''
+DISTRICT_INFO_TABLE = 'district_info'
+def UpdateDistrictInfoFromAnjuke(DB, Logger):
+	sql_string = 'SELECT city_id,city_name,anjuke_rent_url FROM city_info'
+	if not DB.query(sql_string):
+		Logger.error("sql[%s], error[%s]", sql_string, DB.get_last_error())
+		return False
+	city_zuurl_list = DB.fetch_all()		
+	for row in city_zuurl_list:
+		print(row)
+		city_id = row['city_id']
+		zu_url = row['anjuke_rent_url']
+		html = DownloadPage(zu_url)	
+		soup = BeautifulSoup(html,'html.parser')
+		list_soup = soup.find('div', attrs={'class':'sub-items sub-level1'})
+		if list_soup is None:
+			list_soup = soup.find_all('div', attrs={'class':'items'})[0]
+		a_soup_list = list_soup.find_all('a')
+		a_soup_list.remove(a_soup_list[0])
+		for a_soup in a_soup_list:
+			district_name = a_soup.getText().strip()
+			fields = {
+				'city_id':city_id,
+				'district_name': district_name
+			}
+			print(fields)
+			sql_string = f"SELECT district_id FROM district_info where district_name = '{district_name}'"        
+			if not DB.query(sql_string):
+				Logger.error("sql[%s], error[%s]", sql_string, DB.get_last_error())
+				continue
+			rows = DB.fetch_all()		
+			if len(rows) <= 0: 			
+				if DB.insert(DISTRICT_INFO_TABLE, fields):                
+					Logger.info("insert table[%s] district_name[%s], district_name[%s] ok!" \
+					% (DISTRICT_INFO_TABLE, fields["district_name"], fields['district_name']));            
+				else:            
+					Logger.error("insert table[%s] district_name[%s], district_name[%s] error!" \
+					% (DISTRICT_INFO_TABLE, fields["district_name"], fields['district_name'])); 
+					continue
+			else:
+				district_id = rows[0]['district_id']
+				if DB.update(DISTRICT_INFO_TABLE, fields, "district_id = '%s'" % district_id ):
+					if DB.rowcount() == 1:
+						Logger.info("update table[%s] successfully!" % DISTRICT_INFO_TABLE);
+				else:
+					Logger.error("update table[%s] error! sqlerror[%s]" % (DISTRICT_INFO_TABLE, DB.get_last_error()));		
+	pass
+'''
+Update rent housing information
+'''
+
+
 if __name__ == '__main__':
 	Logger = common.CommonHelper.GetLogger('log/%s.log' % (time.strftime("%Y%m%d")));
-	DB = common.MySQLHelper.MySQL(
-		'127.0.0.1', 
-		'root', 
-		'', 
-		3306, 
-		'house_info'
-	)
-	DB.query('SET NAMES UTF8')
-	
-	UpdateTable_city_info(DB, Logger, ANJUKE_CITY_INFO_URL)
-	
+	DB = common.MySQLHelper.MySQL('127.0.0.1', 'root','', 3306, 'house_info')
+	DB.query('SET NAMES UTF8')	
+	#UpdateCityInfoFromAnjuke(DB, Logger, ANJUKE_CITY_INFO_URL)
+	UpdateDistrictInfoFromAnjuke(DB, Logger)
 	
 	
 	
